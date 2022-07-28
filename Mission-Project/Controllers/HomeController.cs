8a1bf7fe34aae25b7f7212de26b431f303fd40fa
Test Test <test@test.com>
2022-07-28 23:08:08 +0300

    Add project files.

---
+++ Mission-Project/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission_Project.Models;
+using System.Diagnostics;
+
+namespace Mission_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(Person person)
+        {
+            // Teknoloji Hesaplama
+
+            // Tc Kimlik Doğrulama 
+
+
+            //if (person.age <= 18 && person.technologies < 25 && person.experienced < 1 && identity == false)
+            //{
+            //    ViewBag.Message =  "AutoRejected";
+            //}
+            //if(person.age >=18 person.technologies > 75 && person.experienced > 1 && identity == true)
+            //{
+            //    ViewBag.Message = "AutoAccepted";
+            //}
+            //if(person.age<=18)
+            //{
+            //    ViewBag.Message = "TransferredToHR";
+            //}
+            //if(person.technologies > 25 && person.technologies < 50  && person.experienced >= 1 && person.experienced <= 2)
+            //{
+            //    ViewBag.Message = "TransferredToLead";
+            //}
+            //if (person.technologies > 50 && person.technologies < 75 && person.experienced >= 2)
+            //{
+            //    ViewBag.Message = "TransferredToCTO";
+            //}
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission-Project/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations; //veri tipinin özelliklerini belirtmek için required gibi.
+
+namespace Mission_Project.Models
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        [Range (0,100)]
+
+        [Required(ErrorMessage = "Please enter your age")]
+        public byte age { get; set; }
+        //[Required(ErrorMessage = "Choose your technologies")]
+        //public string[] technologies { get; set; }
+        //[Required(ErrorMessage = "Text your identityNumber")]
+        //[MaxLength (11)]
+        //[MinLength (11)]
+        //public long identityNumber { get; set; }
+        //[Required(ErrorMessage = "How many work experience")]
+        //public byte experiencedWork { get; set; }
+    }
+}
