887cd9b6de78e20ede09fcf69548d26474d5b806
Test Test <test@test.com>
2022-07-30 15:29:36 +0300

    Proje completed except test class

--- Mission-Project/Controllers/HomeController.cs
+++ Mission-Project/Controllers/HomeController.cs
@@ -13,75 +13,95 @@ namespace Mission_Project.Controllers
         {
             _logger = logger;
         }
-
         public IActionResult Index()
         {
-            ////Creating object of CheckBoxList model class
-            //Person person = new Person();
-            ////Adding items to the list
-            //List<CheckBoxModel> technology = new List<CheckBoxModel>()
-            //{
-            //    new CheckBoxModel { Value = 1, Text = "C#", IsChecked = true },
-            //    new CheckBoxModel { Value = 1, Text = "HTML", IsChecked = false },
-            //    new CheckBoxModel { Value = 1, Text = "JS", IsChecked = false },
-            //    new CheckBoxModel { Value = 1, Text = "MSSQL", IsChecked = false },
-            //    new CheckBoxModel { Value = 1, Text = "ALGORITHM", IsChecked = false },
-            //    new CheckBoxModel { Value = 1, Text = "DATA STRUCTURE", IsChecked = false },
-            //};
-           
-            ////assigning records to the CheckBoxItems list
-            //person.Technologies = technology;
-            //return View(person);
-            return View();
+            //Creating object of Person class
+            Person person = new Person();
+            //Adding items to the list
+            List<Technology> technology = new List<Technology>()
+            {
+                new Technology { Value = 1, Text = "C#", IsChecked = true },
+                new Technology { Value = 1, Text = "HTML", IsChecked = false },
+                new Technology { Value = 1, Text = "JS", IsChecked = false },
+                new Technology { Value = 1, Text = "MSSQL", IsChecked = false },
+                new Technology { Value = 1, Text = "ALGORITHM", IsChecked = false },
+                new Technology { Value = 1, Text = "DATA STRUCTURE", IsChecked = false },
+            };
+            //assigning records to the CheckBoxItems list
+            person.Technologies = technology;
+            return View(person);
         }
 
         [HttpPost]
         public IActionResult Index(Person person)
         {
-            //double percentage = 0;
-            int[] numbers=person.identityNumber.ToString().ToCharArray().Select(Convert.ToInt32).ToArray();  
-            bool idConfirmed = true;
+            double percentage = 0;
+            bool confirmed = true;
+            string TC = Convert.ToString(person.IdentificationNumber);
+            int odd = 0; int even = 0;
+            int tenth = Convert.ToInt32(TC[9].ToString());
+            int eleventh = Convert.ToInt32(TC[10].ToString());
 
             if (ModelState.IsValid)
             {
-                // Teknoloji Hesaplama
+                if (person.Technologies != null)
+                {
+                    foreach (var item in person.Technologies)
+                    {
+                        if (item.IsChecked)
+                        {
+                            percentage += 16.66666666666667;
+                        }
+                    }
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        if (i != 10)
+                        {
+                            odd = (odd + Convert.ToInt32(TC[i].ToString())); // 7 ile çarpılacak sayıları topluyoruz
+                        }
+                    }
+                    else
+                    {
+                        if (i != 9)
+                        {
+                            even = (even + Convert.ToInt32(TC[i].ToString()));
+                        }
+                    }
+                }
 
-                //if (person.Technologies != null)
-                //{
-                //    foreach (var item in person.Technologies) // toplam check olanların sayısı
-                //    {
-                //        if (item.IsChecked)
-                //        {
-                //            percentage += 16.66666666666667;
-                //        }
-                //    }
-                //}
-                // T.C Kimlik Numarası Doğrulama
-                //İşlemler
-                //if (person.Age < 18 && percentage < 25 && experience < 1 && idConfirmed == false)
-                //{
-                //    ViewBag.Loc("AutoRejected");
-                //}
-                //if (person.Age > 18 && percentage > 75 && experience > 1 && idConfirmed == true)
-                //{
-                //    ViewBag.Loc("AutoAccepted");
-                //}
-                //if (person.Age < 18)
-                //{
-                //    ViewBag.Message = "TransferredToHR";
-                //    return View(person);
-                //}
-                //if (percentage > 25 && percentage < 50 && experience > 1 && experience < 2)
-                //{
-                //    ViewBag.Loc("TransferredToLead");
-                //}
-                //if (percentage > 50 && percentage < 75 && experience > 2)
-                //{
-                //    ViewBag.Loc("TransferredToCTO");
-                //}
-                return View(person);
+                if ((((odd * 7) + (even * 9)) % 10 == tenth) && ((odd * 8) % 10 == eleventh))
+                {
+                    confirmed = true;
+                }
+                else
+                {
+                    confirmed = false;
+                }
+                if (person.Age < 18 && percentage < 25 && person.Experience < 1 && confirmed == false)
+                {
+                    TempData["msg"] = "<script>alert('AutoRejected');</script>";
+                }
+                else if (person.Age > 18 && percentage > 75 && person.Experience > 1 && confirmed == true)
+                {
+                    TempData["msg"] = "<script>alert('AutoAccepted');</script>";
+                }
+                else if (person.Age < 18)
+                {
+                    TempData["msg"] = "<script>alert('TransferredToHR');</script>";
+                }
+                else if (percentage >= 25 && percentage <= 50 && person.Experience >= 1 && person.Experience <= 2)
+                {
+                    TempData["msg"] = "<script>alert('TransferredToLead');</script>";
+                }
+                else if (percentage >= 50 && percentage <= 75 && person.Experience > 2)
+                {
+                    TempData["msg"] = "<script>alert('TransferredToCTO');</script>";
+                }
             }
-            return RedirectToAction("Index");
+            return View(person);
         }
         public IActionResult Privacy()
         {
--- Mission-Project/Models/Person.cs
+++ Mission-Project/Models/Person.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering; // Select List Item
-using System.ComponentModel.DataAnnotations; //veri tipinin özelliklerini belirtmek için required gibi.
-
-namespace Mission_Project.Models
+﻿namespace Mission_Project.Models
 {
-    public class CheckBoxModel
+    public class Technology
     {
         public byte Value { get; set; }
         public string? Text { get; set; }
@@ -11,18 +8,10 @@ namespace Mission_Project.Models
     }
     public class Person
     {
-        [Key]
         public int PersonId { get; set; }
-        //[Required]
-        //[Required(ErrorMessage = "Please, enter your age.")]
-        //[Range(1,100, ErrorMessage ="Plase, assign 1 between 100")]
-        //public byte Age { get; set; }
-        //public List<CheckBoxModel>? Technologies { get; set; }
-        [Required(ErrorMessage ="You must sign your ID.")]
-        //[MaxLength(11, ErrorMessage = "Your ID must be 11 characters.")]
-        [Range(10000000000, 99999999999, ErrorMessage = "Your Identity Number must be 11 characters.Not more or less.")]
-        public long identityNumber { get; set; }
-        //[Required(ErrorMessage = "How many work experience")]
-        //public byte experiencedWork { get; set; }
+        public byte Age { get; set; }
+        public List<Technology>? Technologies { get; set; }
+        public long IdentificationNumber { get; set; }
+        public byte Experience { get; set; }
     }
 }
